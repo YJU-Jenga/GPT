15c17343f018ae5e4997179bf121a0f4550d4520
Donggeun <ezfood3@gmail.com>
2023-03-22 20:00:45 +0900

    test

--- GPT_Kinou2.py
+++ GPT_Kinou2.py
@@ -1,6 +1,8 @@
 import openai
 import pyaudio
 import config
+import multiprocessing
+import time
 import speech_recognition as sr
 from gtts import gTTS
 from playsound import playsound
@@ -28,6 +30,7 @@ with sr.Microphone() as source:
                     tts.save("gtts.mp3")
                     playsound("gtts.mp3")
                 else:
+                    start = time.time()
                     response = openai.Completion.create(
                         model="text-davinci-003",
                         # 사용할 GPT-3 모델의 ID를 나타냅니다. 이 값은 OpenAI API에서 제공되는 모델 ID 중 하나여야 합니다. 위 코드에서는 text-davinci-003 모델을 사용하고 있습니다. 이 모델은 가장 성능이 뛰어난 모델 중 하나입니다.
@@ -39,8 +42,10 @@ with sr.Microphone() as source:
                         frequency_penalty=0.0,  # 자주 등장하는 단어를 사용하지 않도록 하는 정도를 나타내는 값입니다. 값이 높을수록 자주 등장하는 단어를 사용하지 않도록 강제됩니다.
                         presence_penalty=0.6,
                         # 모델이 이전에 생성한 단어를 기억하고 다음 단어를 결정하는 정도를 나타내는 값입니다. 값이 높을수록 이전에 생성한 단어와 다르게 텍스트가 생성됩니다.
-                        stop=[" Human:", " AI:"]  # 생성된 텍스트의 끝나는 지점을 나타내는 문자열 리스트입니다. 이 값이 나타날 때까지 모델이 텍스트를 생성합니다.
+                        # stop=[" Human:", " AI:"]  # 생성된 텍스트의 끝나는 지점을 나타내는 문자열 리스트입니다. 이 값이 나타날 때까지 모델이 텍스트를 생성합니다.
                     )
+                    end = time.time()
+                    print(f"{end - start:.5f} sec")
 
                     message = response.choices[0].text.strip()
                     print('message: ', message)
@@ -48,6 +53,12 @@ with sr.Microphone() as source:
                     tts = gTTS(text=message, lang="ko")
                     tts.save("gtts.mp3")
 
-                    playsound("gtts.mp3")
+                    # play = multiprocessing.Process(target=playsound, args='gtts.mp3')
+                    # play.start()
+                    # audio = r.listen(source)
+                    # text = r.recognize_google(audio, language='ko-KR')
+                    # if name in text:
+                    #     play.stop()
+
         except:
             print("Could not recognize your voice")
