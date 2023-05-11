4cc997b7db785b5f788904d060f2e47735a0e043
ASTER18K <donhon05@gmail.com>
2023-05-11 19:25:20 +0900

    asdfasdfasdfasdfasdfasdfasdfasdfasdfsadfasdfasdfasdfasdfasdfasdfas

--- youtube2.py
+++ youtube2.py
@@ -15,7 +15,7 @@ def get_youtube_url(video_title):
 # 음성만 추출하는 함수
 def extract_audio(video_url):
     # , use_oauth = True, allow_oauth_cache = True
-    # allow_oauth_cache = False 할 경우 구글 계정 다시 로그인 해야 함. 기본값 False
+    # allow_oauth_cache = False 할 경우 구글 계정 다시 로그인 해야 함. 기본값 True
     yt = pytube.YouTube(video_url)
     print(yt)
     stream = yt.streams.filter(only_audio=True).first()
