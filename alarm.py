8db2c86a537d28d409bd89b580b8a08082a28755
Donggeun <ezfood3@gmail.com>
2023-05-17 01:07:41 +0900

    update alarm.py

--- alarm.py
+++ alarm.py
@@ -1,8 +1,14 @@
 import requests
 
-url = 'http://ichigo.aster1sk.com:5000/uploads/music/Part_02_1684224610147.mp3'
+# url = 'http://ichigo.aster1sk.com:5000/uploads/music/Part_02_1684224610147.mp3'
 filename = 'alarm.mp3'
+url = 'http://ichigo.aster1sk.com:5000/alarm/getAll/1'
 
 response = requests.get(url)
+data = response.json()
+
+# JSON 객체 출력
+print(data)
+
 with open(filename, 'wb') as file:
     file.write(response.content)
