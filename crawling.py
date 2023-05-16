65f5105cce11cc30a24083f93ee0d9b5100dc3dd
Donggeun <ezfood3@gmail.com>
2023-05-16 19:12:35 +0900

    update crawling.py

--- crawling.py
+++ crawling.py
@@ -1,34 +1,29 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from tqdm import tqdm
+from selenium.webdriver.chrome.options import Options
 import re
+from tqdm import tqdm
 import time
 import pymysql
 import config
 
 
 def get_book_titles():
-    # Set Chrome options
-    chrome_options = Options()
-    # chrome_options.add_argument("--headless") # 브라우저가 뜨지 않고 실행됩니다.
-    # chrome_options.add_argument("--disable-gpu") # Disable GPU rendering, 하드웨어 가속 안함.
-    # chrome_options.add_argument("--lang=ko-Kr") # 사용언어 한국어
-    chrome_options.add_argument('--blink-settings=imagesEnabled=false')  # Do not load images, 브라우저에서 이미지 로딩을 하지 않습니다.
-    chrome_options.add_argument('--mute-audio')  # Mute audio, 브라우저에 음소거 옵션을 적용합니다.
-
-    # Set maximum number of pages to crawl
-    maximum = 20
-
-    # Initialize title and detail lists
+    # Initialize variables
     title_list = []
     detail_list = []
 
+    maximum = 20
+
+    # Set options for Chrome
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+
     # Crawl each page for traditional fairy tales
-    with webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options) as driver:
+    with webdriver.Chrome(options=chrome_options) as driver:
         for i in tqdm(range(1, maximum + 1)):
             URL = f"http://18children.president.pa.go.kr/our_space/fairy_tales.php?srh%5Bcategory%5D=07&srh%5Bpage%5D={i}"
             driver.get(URL)
@@ -48,11 +43,18 @@ def get_book_titles():
                 detail = driver.find_element(By.XPATH, f'//*[@id="content"]/div[2]/div[1]/ul/li[{j}]/dl/dt/a')
                 detail.click()
 
+                # Switch to new tab
+                driver.switch_to.window(driver.window_handles[-1])
+
                 elements_content = driver.find_elements(By.CLASS_NAME, 'content')
                 for element in elements_content:
                     element_text = element.text
                     detail_list.append(element_text)
-                driver.back()
+
+                driver.close()
+
+                # Switch back to original tab
+                driver.switch_to.window(driver.window_handles[0])
 
     # Remove non-Korean characters from titles
     title_replace = [re.sub(r"[^가-힣]", "", title) for title in title_list]
@@ -60,6 +62,20 @@ def get_book_titles():
 
 
 start = time.time()
+
+# Connect to database
+db = pymysql.Connect(
+    host='localhost',
+    user='jenga',
+    password=config.database_password,
+    database='jenga',
+    charset='utf8',
+)
+cursor = db.cursor()
+
+# Database Create Table
+# create_table_query = """
+# CREATE
 
 # Connect to database
 db = pymysql.Connect(
