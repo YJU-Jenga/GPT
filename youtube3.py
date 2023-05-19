bd1ff9cc1db5557549f8fc612741670f7a3d31cc
ASTER18K <donhon05@gmail.com>
2023-05-19 15:08:10 +0900

    youtube3 fix

--- youtube3.py
+++ youtube3.py
@@ -17,7 +17,8 @@ def get_youtube_url(video_title):
 
 # 음성만 추출하는 함수
 def extract_audio(video_url):
-    yt = pytube.YouTube(video_url, use_oauth=True, allow_oauth_cache=True)
+    yt = pytube.YouTube(video_url)
+    print(yt)
     stream = yt.streams.filter(only_audio=True).first()
     output_path = stream.download()
     # pydub 라이브러리를 사용하여 RIFF 형식으로 변환
